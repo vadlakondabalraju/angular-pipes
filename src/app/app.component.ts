5a0714bdfdb322f59570ce5e3ea699741f6ba153
vadlakonda balraju <vadlakondabalraj001@gmail.com>
2023-06-06 18:28:53 +0530

    pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myapp';
+  text: string = 'pipes in angular';
+  users: any[] = [
+    {
+      id: 101,
+      name: 'raju',
+      city: 'HYDERABAD',
+      salary: 20000,
+      dob: new Date('10/12/1988'),
+    },
+    {
+      id: 102,
+      name: 'ravi',
+      city: 'DELHI',
+      salary: 10000,
+      dob: new Date('10/12/1998'),
+    },
+    {
+      id: 103,
+      name: 'ramu',
+      city: 'BANGALORE',
+      salary: 40000,
+      dob: new Date('10/12/1990'),
+    },
+    {
+      id: 104,
+      name: 'rohan',
+      city: 'GOA',
+      salary: 50000,
+      dob: new Date('10/12/1984'),
+    },
+  ];
 }
